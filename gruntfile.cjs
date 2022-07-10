e8cf888a8060418695077fa1134344baf99bb8b5
Honor <admin@honor2045.com>
2022-07-09 21:54:07 -0300

    bug(build): add gruntfile

---
+++ gruntfile.cjs
@@ -0,0 +1,68 @@
+const { basename } = require("path");
+
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        shortName: "spongia",
+        pkg: grunt.file.readJSON("package.json"),
+        sass: {
+            default: {
+                files: [
+                    {
+                        expand: true,
+                        src: "src/main/*.scss",
+                        dest: "dest/",
+                        rename: (dest, src) => { return dest + basename(src) },
+                    }
+                ]
+            }
+        },
+        cssmin: {
+            default: {
+                files: [
+                    {
+                        dest: 'dist/<%= shortName %>.min.css',
+                        src: 'dest/index.scss'
+                    }
+                ]
+            }            
+        },
+        clean: {
+            default: [ 'dist' ],
+            dest: [ 'dest' ],
+        },
+        pug: {
+            default: {
+                files: [
+                    {
+                        expand: true,
+                        src: "src/test/*.pug",
+                        dest: "src/test/",
+                        rename: (dest, src) => { return dest + basename(src).replace(".pug", ".html") },
+                    }
+                ]
+            }
+        },
+        watch: {
+            main: {
+                files: [ "src/main/*.scss" ],
+                tasks: [ 'main:compile' ],
+            },
+            test: {
+                files: [ "src/test/*.pug" ],
+                tasks: [ 'test:compile' ],
+            },
+        }
+    });
+
+    grunt.registerTask('main:compile', [ 'sass', 'cssmin', 'clean:dest' ]);
+    grunt.registerTask('main:watch', [ 'watch:main' ]);
+    grunt.registerTask('test:compile', [ 'pug' ]);
+    grunt.registerTask('test:watch', [ 'watch:test' ]);
+    grunt.registerTask('dist:clean', [ 'clean:dist' ]);
+}
