9b665752bb8680345432c11d7dd83f8d761ac1cd
Luis Float <contact@luisfloat.com>
2022-10-24 17:41:21 -0300

    feat(build): add vitepress config

---
+++ .vitepress/config.js
@@ -0,0 +1,10 @@
+import themeConfig from "../src/docs/themeConfig.js";
+
+export default {
+    title: 'SpongiaCSS',
+    description: 'SpongiaCSS Website',
+    base: '/spongia-css/',
+    srcDir: './src/docs/views/',
+    outDir: './dist/docs/',
+    themeConfig,
+};
