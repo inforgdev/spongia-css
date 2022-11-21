59ff258fe41a6f42d284b11b3979205928d09ae8
Luis Float <contact@luisfloat.com>
2022-11-20 21:21:28 -0300

    feat(docs): set dark theme as default

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,6 +3,7 @@ import themeConfig from "../src/docs/themeConfig.js";
 export default {
     title: 'SpongiaCSS',
     description: 'SpongiaCSS Website',
+    appearance: 'dark',
     base: '/spongia-css/',
     srcDir: './src/docs/views/',
     outDir: './dist/docs/',
